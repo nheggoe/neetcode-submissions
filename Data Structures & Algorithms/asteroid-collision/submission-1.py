eda0f5051fdba4d57183d0faa9a700e8369283dd
Nick Heggø <git@nheggoe.dev>
2026-07-31 11:16:23 +0200

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def asteroidCollision(self, asteroids: list[int]) -> list[int]:
+        alive = []
+        positive = []
+        for a in asteroids:
+            if a < 0:
+                finish = False
+                while not finish:
+                    if len(positive) == 0:
+                        alive.append(a)
+                        finish = True
+                    else:
+                        curr = positive.pop()
+                        if abs(curr) == abs(a):
+                            finish = True
+                        elif abs(curr) > abs(a):
+                            positive.append(curr)
+                            finish = True
+                        else:
+                            continue
+            else:
+                positive.append(a)
+                        
+        return alive + positive
+
+
+
+
+
+            
